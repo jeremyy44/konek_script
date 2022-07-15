d3eaf9f5e4b84a028eea481a1af977030954f824
jeremyy44 <jeremie.roy@live.ca>
2022-07-15 16:14:31 -0400

    Added interfaces and hosts config

--- firstboot/firstboot.sh
+++ firstboot/firstboot.sh
@@ -33,19 +33,23 @@ apt-get -qq install sudo -y
 echo -e "${LG}Sudo installed succesfully!${NC}\n"
 
 # Adds the user to the sudo group
-function install_sudo() {
-        echo -e "The user you created needs to be addes to the sudo group."
-        username=$(who am i | awk '{print $1}')
-        echo -e "Adding user ${BLUE}$username${NC} to sudoers..."
+echo -e "The user you created needs to be addes to the sudo group."
+username=$(who am i | awk '{print $1}')
+echo -e "Adding user ${BLUE}$username${NC} to sudoers..."
     if usermod -aG sudo $username ; then
         echo -e "${LG}Added succesfully!${NC}\n"
     else
         clear
         echo -e "${RED}Failed to add user to sudoers, please try again.${NC}"
-        install_sudo
+        exit
     fi
-}
 
-install_sudo
+echo -e "Adding config to /etc/hosts"
+echo -e "10.11.10.1      branch01\n10.11.10.2      branch02" >> /etc/hosts
+echo -e "${LG}Added succesfully!${NC}\n"
+
+echo -e "Adding config to /etc/network/interfaces"
+cat interface-conf > /etc/network/interfaces
+echo -e "${LG}Added succesfully!${NC}\n"
 
 echo -e "${LG}Now time to bootstrap!${NC}"
