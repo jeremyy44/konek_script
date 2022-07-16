a0ade01bdba232ca3f58760394fe742242147806
jeremyy44 <jeremie.roy@live.ca>
2022-07-16 09:45:07 -0400

    Beautysh run

--- firstboot/firstboot.sh
+++ firstboot/firstboot.sh
@@ -7,7 +7,7 @@ NC='\033[0m' # No Color
 
 # Checks if the user is root
 if [ "$EUID" -ne 0 ]
-    then echo -e "You are not root. Run 'su -' before running this script"
+then echo -e "You are not root. Run 'su -' before running this script"
     exit
 fi
 
@@ -18,13 +18,13 @@ if apt-get -qq update ; then
 else
     echo -e "Failed to update, deleting cdrom from /etc/apt/sources.list and reruning update."
     sed -i '5d' /etc/apt/sources.list
-        if apt-get -qq update ; then
-            echo -e "${LG}Updated succesfully!${NC}\n"
-        else
-            clear
-            echo -e "${RED}Failed to update, please try again.${NC}"
-            exit
-        fi
+    if apt-get -qq update ; then
+        echo -e "${LG}Updated succesfully!${NC}\n"
+    else
+        clear
+        echo -e "${RED}Failed to update, please try again.${NC}"
+        exit
+    fi
 fi
 
 # Installs the necessary packages
@@ -36,14 +36,15 @@ echo -e "${LG}Sudo installed succesfully!${NC}\n"
 echo -e "The user you created needs to be addes to the sudo group."
 username=$(who am i | awk '{print $1}')
 echo -e "Adding user ${BLUE}$username${NC} to sudoers..."
-    if usermod -aG sudo $username ; then
-        echo -e "${LG}Added succesfully!${NC}\n"
-    else
-        clear
-        echo -e "${RED}Failed to add user to sudoers, please try again.${NC}"
-        exit
-    fi
+if usermod -aG sudo $username ; then
+    echo -e "${LG}Added succesfully!${NC}\n"
+else
+    clear
+    echo -e "${RED}Failed to add user to sudoers, please try again.${NC}"
+    exit
+fi
 
+# Apends config to /etc/hosts and /etc/network/interfaces
 echo -e "Adding config to /etc/hosts"
 echo -e "10.11.10.1      branch01\n10.11.10.2      branch02" >> /etc/hosts
 echo -e "${LG}Added succesfully!${NC}\n"
